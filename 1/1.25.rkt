3a5a85c6a375e232454b77aed4f5f90a9c74598a
Jacob <jacobgeorge08@gmail.com>
2023-07-24 09:49:55 +0530

    completed 1.25

---
+++ 1/1.25.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+;Exercise 1.25
+
+;Alyssa P. Hacker complains that we went to a lot of extra work in writing expmod. After all, she says, since we already know how to compute exponentials, we could have simply written
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+; Is she correct? Would this procedure serve as well for our fast prime tester? Explain.
+
+; Answer
+; No. It would produce the same results, but much slower becuase it is inefficient for large exponents
